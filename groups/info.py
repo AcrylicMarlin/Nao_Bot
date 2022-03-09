fc23c743a0241b7d6adfcf1512ad0f08f416a45b
Marlin <50shorty56@gmail.com>
2022-03-09 15:32:08 -0600

    Added Information commands Added an URL agent

---
+++ groups/info.py
@@ -0,0 +1,70 @@
+from optparse import Option
+import typing
+import discord
+from discord import app_commands
+from typing import Optional
+
+
+class Info(app_commands.Group):
+    def __init__(self, client):
+        super().__init__(name = 'info', description='Commands for various information')
+        self.client:discord.Client = client
+
+    @app_commands.command(name = 'bot', description='Information on the bot')
+    async def bot(self, interaction:discord.Interaction):
+        embed = discord.Embed()
+        embed.title = "Nao_Bot Information"
+        member_count = 0
+        for guild in self.client.guilds:
+            member_count += guild.member_count
+        embed.description = f"""
+Mention - {self.client.user.mention}
+Name - {self.client.user.display_name}
+ID - {self.client.user.id}
+Number of guilds - {len(self.client.guilds)}
+Number of members - {member_count}
+"""
+        await interaction.response.send_message(embed = embed)
+        ...
+    @app_commands.command(name = 'member', description='Information on a member')
+    @app_commands.describe(member = "Member to get info of. Defaults to yourself")
+    async def member(self, interaction:discord.Interaction, member:Optional[discord.Member]):
+        if not member:
+            member = interaction.user
+        
+        embed = discord.Embed()
+        embed.title = f"{member.display_name}'s information"
+        role_string = ''
+        for role in member.roles[1:]:
+            role_string += role.mention + ', '
+        
+        embed.description = f"""
+Mention: {member.mention}
+Name: {member.display_name}
+ID: {member.id}
+Joined Server: <t:{int(member.joined_at.timestamp())}:R>
+Joined Discord: <t:{int(member.created_at.timestamp())}:R>
+Roles: {role_string [:-2]}
+"""
+        embed.set_thumbnail(url = member.display_avatar)
+        embed.set_footer(text = f'{member.display_name}', icon_url=member.display_avatar)
+        await interaction.response.send_message(embed = embed)
+        ...
+    
+    @app_commands.command(name='server', description='Gets info on the server')
+    async def server(self, interaction:discord.Interaction):
+
+        embed = discord.Embed()
+        embed.title = f'Info for {interaction.guild.name}'
+
+        embed.description = f"""
+Name - {interaction.guild.name}
+ID - {interaction.guild.id}
+Members - {interaction.guild.member_count}
+Created - <t:{int(interaction.guild.created_at.timestamp())}:R>
+"""
+        embed.set_footer(text=f'{interaction.user.display_name}#{interaction.user.discriminator}', icon_url=interaction.user.avatar)
+        embed.set_thumbnail(url=interaction.guild.icon)
+
+        await interaction.response.send_message(embed=embed)
+        ...
--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import asyncio
-from email.mime import application
-from http import client
 import os
+import json
 
 import discord
 from discord import app_commands
+from discord.ext import commands
 from dotenv import load_dotenv
-import  groups.basic as basic
+import aiohttp
+import groups.basic as basic
+import groups.info as info
+import groups.urls as urls
+
 load_dotenv()
 
 TOKEN = os.environ.get('TOKEN')
+TINYAPITOKEN = os.environ.get('TINYAPITOKEN')
 NAO_NATION_ID = 695025143264706622
+NAO_NATION = discord.Object(id = NAO_NATION_ID)
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -20,13 +26,43 @@ tree = app_commands.CommandTree(client)
 @client.event
 async def on_ready():
     await client.wait_until_ready()
-    await initialise(tree=tree, client=client, NAO_NATION_ID=NAO_NATION_ID)
+    await initialise(tree=tree, client=client, NAO_NATION=NAO_NATION, TINYAPITOKEN=TINYAPITOKEN)
     print('Nao_Bot is operational')
 
 
+@tree.command(name = 'foo', description='Trial command [FOR DEVS ONLY]', guild=NAO_NATION)
+async def foo(interaction:discord.Interaction):
+    '''
+        "body":{
+        "url":interaction.guild.icon.url,
+        "domain":"tiny.one",
+        "alias":f"{interaction.guild.name}icon"
+    '''
+    if interaction.user.id == 846398473115795456 or interaction.user.id == 432613614168571904:
+        data = {
+            "url":"https://media.discordapp.net/attachments/908752890456322110/941409051126951946/IMG_7549.png?width=567&height=676",
+            "domain":"tiny.one",
+            "alias":"randommemefromserver"
+        }
+        
+        async with aiohttp.ClientSession() as session:
+            async with session.post(f"https://api.tinyurl.com/create?api_token={TINYAPITOKEN}", data=data) as response:
 
+                item = await response.json()
+                embed = discord.Embed()
+                embed.title = 'Testing aiohttp Requests'
+                embed.description = f"""
+                ```
+                {json.dumps(item, indent=4)}
+                ```
+                """
+                await interaction.response.send_message(embed=embed)
+                
 
-async def initialise(*, tree:app_commands.CommandTree, client:discord.Client, NAO_NATION_ID:int):
+    else:
+        await interaction.response.send_message("This command is for devs only.")
+
+async def initialise(*, tree:app_commands.CommandTree, client:discord.Client, NAO_NATION:discord.Object, TINYAPITOKEN:str):
     """_summary_
     Initialises the bot
     Adds all of the groups to the client
@@ -37,8 +73,11 @@ async def initialise(*, tree:app_commands.CommandTree, client:discord.Client, NA
         client (discord.Client): _description_
         NAO_NATION_ID (int): _description_
     """
-    tree.add_command(basic.Basic(client), guild=discord.Object(id=NAO_NATION_ID))
-    await tree.sync(guild=discord.Object(id = NAO_NATION_ID))
+    tree.add_command(basic.Basic(client), guild=NAO_NATION)
+    tree.add_command(info.Info(client), guild=NAO_NATION)
+    tree.add_command(urls.Urls(client, TINYAPITOKEN), guild=NAO_NATION)
+
+    await tree.sync(guild=NAO_NATION)
 
 
 if __name__ == '__main__':
